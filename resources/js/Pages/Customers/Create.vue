d7d74a90beb479edf0481cac774f429b28d4446c
Osmell Caicedo <correo.oele@gmail.com>
2024-04-12 17:09:12 -0500

    Add the create view

---
+++ resources/js/Pages/Customers/Create.vue
@@ -0,0 +1,116 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import TextareaInput from '@/Components/TextareaInput.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone: '',
+    observation: '',
+});
+</script>
+
+<template>
+    <Head title="Create Customer" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Create a New Customer</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+                <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                    <form @submit.prevent="form.post(route('customers.store'))" class="mt-6 space-y-6">
+                        <div>
+                            <InputLabel for="first_name" value="First Name" />
+                            <TextInput
+                                id="first_name"
+                                type="text"
+                                class="block w-full mt-1"
+                                v-model="form.first_name"
+                                required
+                                autofocus
+                                autocomplete="first_name"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.first_name" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="last_name" value="Last Name" />
+                            <TextInput
+                                id="last_name"
+                                type="text"
+                                class="block w-full mt-1"
+                                v-model="form.last_name"
+                                required
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.last_name" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="email" value="Email" />
+
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="block w-full mt-1"
+                                v-model="form.email"
+                                required
+                                autocomplete="username"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="phone" value="Phone Number" />
+                            <TextInput
+                                id="phone"
+                                type="text"
+                                class="block w-full mt-1"
+                                v-model="form.phone"
+                                required
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.phone" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="observation" value="Observation" />
+
+                            <TextareaInput
+                                id="observation"
+                                class="block w-full mt-1"
+                                v-model="form.observation"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.observation" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                            <Transition
+                                enter-active-class="transition ease-in-out"
+                                enter-from-class="opacity-0"
+                                leave-active-class="transition ease-in-out"
+                                leave-to-class="opacity-0"
+                            >
+                                <p v-if="form.recentlySuccessful" class="text-sm text-gray-600 dark:text-gray-400">Saved.</p>
+                            </Transition>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
