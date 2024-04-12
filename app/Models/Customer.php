60ed60c0f16a5dfdf7df05fb7b4c8f66e5f06146
Osmell Caicedo <correo.oele@gmail.com>
2024-04-12 17:09:31 -0500

    Allow to search customers

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,5 +23,12 @@ class Customer extends Model
     public function getFullnameAttribute(): string
     {
         return $this->first_name.' '.$this->last_name;
+    }
+
+    public function scopeFilter(Builder $query, string $value = null): Builder
+    {
+        return $query->when($value, function ($q, $v) {
+            $q->where('first_name', 'like', "%{$v}%")->orWhere('last_name', 'like', "%{%$v}%");
+        });
     }
 }
