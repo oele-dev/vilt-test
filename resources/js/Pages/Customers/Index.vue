df3babf643f20a9d13866d085bfd72751dd541b2
Osmell Caicedo <correo.oele@gmail.com>
2024-04-12 14:55:36 -0500

    add pagination component

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -1,8 +1,9 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
 import { Head } from '@inertiajs/vue3';
 
-defineProps({ customers: Array })
+defineProps({ customers: Object })
 
 </script>
 
@@ -21,18 +22,18 @@ defineProps({ customers: Array })
                         <thead class="bg-gray-200 border-b">
                             <tr>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">#</th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">First</th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Last</th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">FirstName</th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">LastName</th>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Email</th>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Phone</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr
-                                v-for="(customer, k) in customers"
+                                v-for="(customer) in customers.data"
                                 :key="customer.id"
                                 class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ k+1 }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ customer.index }}</td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ customer.first_name }}</td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ customer.last_name }}</td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ customer.email }}</td>
@@ -41,6 +42,7 @@ defineProps({ customers: Array })
                         </tbody>
                     </table>
                 </div>
+                <pagination class="mt-6" :links="customers.links" />
             </div>
         </div>
     </AuthenticatedLayout>
